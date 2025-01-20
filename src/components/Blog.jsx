df39354f2f3085f29524ef0b499a6cf41d8c5bb4
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-20 16:21:37 +0200

    Tehtävä 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,48 @@
-const Blog = ({ blog }) => (
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [buttonLabel, setButtonLabel] = useState('view')
+  const [opened, setOpened] = useState(false)
+
+  const toggle = () => {
+    if (opened) {
+    setButtonLabel('view')
+    setOpened(false)
+    } else {
+    setButtonLabel('hide')
+    setOpened(true)
+    }
+  }
+
+  const like = () => {
+    console.log('liked')
+  }
+
+  return(
+    <div style={blogStyle}>
+    {!opened &&
   <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+    {blog.title} {blog.author} <button onClick={toggle}>{buttonLabel}</button>
+  </div>}
+  {opened &&
+  <div>
+    <div>
+      {blog.title} {blog.author} <button onClick={toggle}>{buttonLabel}</button>
+    </div>
+    <div>{blog.url}</div>
+    <div>{blog.likes} <button onClick={like}>like</button></div>
+    <div>{blog.user.name}</div>
+  </div>}
+  </div>
+    ) 
+}
 
 export default Blog
